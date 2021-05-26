141b9405fe9928449dfaff31895f0a7c6a72c270
MargVander <margauxvanderbeken@gmail.com>
2021-05-26 11:51:59 +0200

    fetch popular bars

--- src/modules/bars/barList.vue
+++ src/modules/bars/barList.vue
@@ -1,5 +1,12 @@
 <template>
   <ion-content class="test">
+     <ion-item class="select">
+      <ion-label>Trier par :</ion-label>
+      <ion-select @ionChange="onChange($event)" :value="sort">
+        <ion-select-option :value="0">Plus récent</ion-select-option>
+        <ion-select-option :value="1">Popularité</ion-select-option>
+      </ion-select>
+    </ion-item>
     <BarCard :data="bars" />
     <div class="xtraspace" />
     <Tab :currentPage="'search'" />
@@ -11,17 +18,21 @@ import BarCard from "./components/barCard";
 import BarService from "./services/BarService";
 import Tab from "../home/components/tab";
 
-import { IonContent } from "@ionic/vue";
+
+import { IonContent, IonSelect, IonSelectOption } from "@ionic/vue";
 
 export default {
   components: {
     BarCard,
-    Tab
+    Tab,
+    IonSelect,
+    IonSelectOption
   },
   data() {
     return {
       bars: [],
-      rating: null
+      rating: null,
+      sort: 0,
     };
   },
   methods: {
@@ -29,10 +40,36 @@ export default {
       BarService.fetchBars().then(bars => {
         this.bars = bars;
       });
+    },
+    fetchBarsByPopularity() {
+      BarService.fetchBarsByPopularity().then(bars => {
+        this.bars = bars;
+      });
+    },
+    onChange(event) {
+      if (event.detail.value === 0) {
+        this.fetchBars()
+        this.sort = 0
+      } else {
+        this.fetchBarsByPopularity()
+        this.sort = 1
+      }
     }
   },
   created() {
     this.fetchBars();
-  }
+  },
 };
 </script>
+<style>
+.select {
+  margin: 0 20px;
+}
+.select h2{
+  color: black;
+}
+.alert-title.sc-ion-alert-ios, .alert-radio-label.sc-ion-alert-ios {
+  color: black;
+}
+
+</style>
